7981c77bbb0deb00d60cfb2e33efb33a5dfe51bd
zunath <coolty3001@yahoo.com>
2018-02-01 20:32:46 +0800

    Stub quest editor out

---
+++ CZS.Web/ViewModels/QuestEditorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CZS.Web.Attributes;
+using CZS.Web.Constants;
+using DotNetify;
+
+namespace CZS.Web.ViewModels
+{
+    [RoleRequired(RoleType.Admin)]
+    public class QuestEditorViewModel: BaseVM
+    {
+
+    }
+}
