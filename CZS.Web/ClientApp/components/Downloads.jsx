41edc069b9dd191d448d22df074f0ef6aff79907
zunath <coolty3001@yahoo.com>
2018-03-20 13:26:50 +0800

    Update home page to include links to buy NWN: EE. Update downloads page to include link to the Steam workshop page.

--- CZS.Web/ClientApp/components/Downloads.jsx
+++ CZS.Web/ClientApp/components/Downloads.jsx
@@ -17,35 +17,66 @@ export default class Downloads extends React.Component {
             <div>
                 
                 <h2 className="center">Downloads</h2>
-                <div className="row">
-                    <table className="table striped bordered">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Description</th>
-                                <th>Link</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.DownloadList.map(function (obj) {
-                                return <tr key={obj.DownloadId}>
-                                           <td>
-                                               {obj.Name}
-                                           </td>
-                                           <td>
-                                               {obj.Description}
-                                           </td>
-                                           <td>
-                                               <a className="btn btn-outline-primary" href={obj.Url}>
-                                                   Download
-                                               </a>
+                <div className="row">&nbsp;</div>
+                <div className="card border-primary mb-3">
+                    <div className="card-body">
+                        <h4 className="card-title center">Automatic Downloads</h4>
+                        <p className="card-text">
+                            <p>
+                                The recommended way to download our files is to subscribe to us on Steam. Simply click the button below, click "Subscribe" and the files will download automatically!
+                            </p>
 
-                                           </td>
-                                       </tr>;
-                            })}
-                        </tbody>
-                    </table>
+                            <a className="btn btn-outline-primary btn-block" target="_blank" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1323035693">
+                                Subscribe on Steam
+                            </a>
+                         </p>
+                    </div>
                 </div>
+
+                <div className="row">&nbsp;</div>
+
+
+                <div className="card border-primary mb-3">
+                    <div className="card-body">
+                        <h4 className="card-title center">Manual Downloads</h4>
+                        <p className="card-text">
+                            <p>
+                                Don't use Steam, or prefer to manage the files yourself? No problem! Download all of the files below and place them into the appropriate location.
+                            </p>
+
+                            <div className="row">
+                                <table className="table striped bordered">
+                                    <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Description</th>
+                                        <th>Link</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {this.state.DownloadList.map(function (obj) {
+                                            return <tr key={obj.DownloadId}>
+                                                <td>
+                                                    {obj.Name}
+                                                </td>
+                                                <td>
+                                                    {obj.Description}
+                                                </td>
+                                                <td>
+                                                    <a className="btn btn-outline-primary" href={obj.Url}>
+                                                        Download
+                                                    </a>
+
+                                                </td>
+                                            </tr>;
+                                        })}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </p>
+                    </div>
+                </div>
+                
             
             </div>
         );
--- CZS.Web/ClientApp/components/Home.jsx
+++ CZS.Web/ClientApp/components/Home.jsx
@@ -18,11 +18,14 @@ export default class Home extends React.Component {
             <div>
 
                 <h1 className="center">Welcome to Cyberpunk Zombie Survival!</h1>
-                <h3 className="center">A NeverWinter Nights Server</h3>
-                <h5 className="center">
+                <h3 className="center">A Neverwinter Nights: Enhanced Edition Server</h3>
+                <h4 className="center">
                     Direct Connect: czs.eastus.cloudapp.azure.com:5121 <Link to="/downloads"><i>(Download Files)</i></Link>
-                </h5>
+                </h4>
 
+                <h5 className="center">
+                <i><a href="http://store.steampowered.com/app/704450/Neverwinter_Nights_Enhanced_Edition/" target="_blank">Buy NWN today on Steam</a></i> or <i><a href="https://www.beamdog.com/products/neverwinter-nights-enhanced-edition#signup-form" target="_blank">directly from Beamdog! </a></i>
+                </h5>
 
                 <Carousel showArrows={true} autoPlay={true} infiniteLoop={true} useKeyboardArrows={true} stopOnHover={true} dynamicHeight={true}>
                     <div>
